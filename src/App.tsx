403dc6ea6083ca99d30d2b76c53e127dcf59f670
mobaibai <puresilent.com@gmail.com>
2023-04-09 20:58:40 +0800

    ✨ 添加动画配置

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,35 @@ import { HashRouter } from 'react-router-dom'
 import vhCheck from 'vh-check'
 import RouterConainer from './router'
 import { Header } from '@/components/Header'
+import { useCountStore } from '@/stores/useCountStore'
 import 'virtual:uno.css'
 import 'virtual:svgsprites'
 import './styles/global.scss'
 import './styles/app.scss'
+import { useSpring, animated } from '@react-spring/web'
 
 vhCheck()
 
 function App() {
+  const count = useCountStore(state => state.count)
+  const countStyles = useSpring({
+    from: { transform: 'rotateZ(0)' },
+    loop: { transform: 'rotateZ(360deg)' },
+    to: { transform: 'rotateZ(0)' },
+    config: {
+      duration: 200
+    }
+  })
+
   return (
     <div className="App">
       <div className="limit">
         <HashRouter>
           <Header />
           <RouterConainer />
+          <animated.div className='count-view' style={{ ...countStyles }} text="center 30px slate-600">
+            {count}
+          </animated.div>
         </HashRouter>
       </div>
     </div>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react"
 import { Button } from "antd"
 import { useCountStore } from "@/stores/useCountStore"
 import { apiGetHomeData } from "./request"
@@ -8,20 +7,14 @@ interface Props {
 }
 const Home: React.FC<Props> = (props) => {
   if (props.title) document.title = props.title
-  const count = useCountStore(state => state.count)
   const countInc = useCountStore(state => state.inc)
   const countCut = useCountStore(state => state.cut)
 
-  useEffect(() => {
-    console.log('apiGetHomeData', apiGetHomeData())
-  }, [])
-
   return (
     <div className="home-container" p-20px>
-      <div className="count-action" flex items-center justify-center>
-        <Button onClick={countInc}>+</Button>
-        <div mx-20px>{count}</div>
+      <div className="count-action" flex items-center justify-center children-mx-10px>
         <Button onClick={countCut}>-</Button>
+        <Button onClick={countInc}>+</Button>
       </div>
     </div>
   )
