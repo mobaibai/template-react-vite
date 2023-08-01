e6db3a88bf60bdaae95e53b49bbbd60b2f290d24
mobaibai <puresilent.com@gmail.com>
2023-08-01 23:41:55 +0800

    ✨ feat: 跟随导航相关封装完成

--- src/components/NavFollow.tsx
+++ src/components/NavFollow.tsx
@@ -1,4 +1,3 @@
-import type { CSSProperties } from 'react'
 import { useEffect, useRef, useState } from "react"
 import { nanoid } from 'nanoid'
 import { Empty } from 'antd'
@@ -6,30 +5,38 @@ import { Empty } from 'antd'
 interface NavFollowProps<T> {
   list: Array<T>
   navItemName: string
-  navTextColor?: string
-  navTextActiveColor?: string
-  followType: 'bg' | 'border'
-  followColor?: string
-  onClickNavItemHandler: (item: T) => void
+  size?: 'small' | 'default' | 'large'
+  followType?: 'bg' | 'border'
+  color?: string
+  onClickNavItemHandler?: (item: T) => void
 }
 /**
  * @description: 跟随导航
  * @params props {
     * @param {type} list List数据
     * @param {type} navItemName NavItem字段名
-    * @param {type} navTextColor NavItem文字颜色
-    * @param {type} navTextActiveColor NavItem文字高亮颜色
+    * @param {type} size 大小
     * @param {type} followType 跟随Bar类型
-    * @param {type} followColor 跟随Bar颜色
+    * @param {type} color 颜色
     * @param {type} onClickNavItemHandler NavItem点击事件
  * }
  * @return {type}
  */
-export const NavFollow: React.FC<NavFollowProps<any>> = ({ list, navItemName, navTextColor = 'text-gray-600', navTextActiveColor = 'text-[var(--theme-primary)]', followColor = 'bg-gray-200', followType, onClickNavItemHandler }) => {
+export const NavFollow: React.FC<NavFollowProps<any>> = ({
+  list,
+  navItemName,
+  size = 'default',
+  followType = 'border',
+  color = 'var(--theme-primary)',
+  onClickNavItemHandler = () => { } }) => {
   const [navIndex, useNavIndex] = useState<number>(0)
   const navIndexRef = useRef<number>(0)
   const followItemRef = useRef<HTMLDivElement>(null)
   const navItemRefs = list.map(() => useRef<HTMLDivElement>(null))
+
+  const fontSize = size === 'small' ? 'text-[12px]' : size === 'default' ? 'text-[14px]' : 'text-[16px]'
+  const borderHeight = size === 'small' ? 'border-b-[3px]' : size === 'default' ? 'border-b-[4px]' : 'border-b-[5px]'
+  const borderColor = `border-[${color}]`
 
   useEffect(() => {
     navAssignment()
@@ -65,9 +72,9 @@ export const NavFollow: React.FC<NavFollowProps<any>> = ({ list, navItemName, na
    */
   const navAssignment = () => {
     const navItem = navItemRefs[navIndexRef?.current]?.current
-    const width = followType === 'bg' ? navItem && navItem?.offsetWidth : navItem && navItem?.offsetWidth - 40
+    const width = followType === 'bg' ? navItem?.offsetWidth : navItem && navItem?.offsetWidth - 20 * 2
     const height = navItem?.offsetHeight
-    const left = followType === 'bg' ? navItem && navItem?.offsetLeft : navItem && navItem?.offsetLeft + 20
+    const left = followType === 'bg' ? navItem?.offsetLeft : navItem && navItem?.offsetLeft + 20
     const top = navItem?.offsetTop
     const followItem = followItemRef.current
     if (followItem) {
@@ -85,7 +92,7 @@ export const NavFollow: React.FC<NavFollowProps<any>> = ({ list, navItemName, na
           list && list.length
             ?
             list.map((item: any, index: number) => (
-              <div key={nanoid()} ref={navItemRefs[index]} className={`nav-item z-10 mr-[44px] mb-[40px] px-[20px] py-[8px] rounded-md text-[14px] transition-all duration-200 ${navIndex === index ? `${navTextActiveColor} font-bold` : `${navTextColor} hover:${navTextActiveColor} hover:cursor-pointer`}`} onClick={() => onClickNav(index)}>{item[navItemName]}</div>
+              <div key={nanoid()} ref={navItemRefs[index]} className={`nav-item z-10 mr-[12px] mb-[20px] px-[20px] py-[8px] rounded-md ${fontSize} transition-all duration-200 ${navIndex === index ? `font-bold` : `text-gray-600 hover:text-gray-800 hover:font-bold hover:cursor-pointer`}`} onClick={() => onClickNav(index)} style={{ color: `${navIndex === index ? color : ''}` }}>{item[navItemName]}</div>
             ))
             :
             (
@@ -94,7 +101,7 @@ export const NavFollow: React.FC<NavFollowProps<any>> = ({ list, navItemName, na
               </div>
             )
         }
-        <div ref={followItemRef} className={`follow-item absolute z-0 transition-all duration-500 ${followType === 'bg' ? `${followColor}  rounded-md` : 'border-b-[4px] border-[var(--theme-primary)]'}`} />
+        <div ref={followItemRef} className={`follow-item absolute z-0 transition-all duration-500 ${followType === 'bg' ? 'bg-gray-200 rounded-md' : `${borderHeight}`}`} style={{ borderColor: color }} />
       </div>
     </div>
   )
--- src/pages/video/play1/index.tsx
+++ src/pages/video/play1/index.tsx
@@ -1,69 +1,18 @@
-import { useEffect, useRef, useState } from "react"
-import { nanoid } from "nanoid"
-import { Empty, Skeleton } from "antd"
+import { Skeleton } from "antd"
 import { NavFollow } from "@/components/NavFollow"
 
 interface NavItem {
   id: string | number
-  fruit: string
+  fruit?: string
+  name?: string
 }
-const navList: Array<NavItem> = [{ id: '-1', fruit: '全部' }, { id: '1', fruit: '香蕉' }, { id: '2', fruit: '苹果' }, { id: '3', fruit: '橙子' }, { id: '4', fruit: '草莓' }, { id: '5', fruit: '蓝莓' }, { id: '6', fruit: '菠萝' }, { id: '7', fruit: '樱桃' }, { id: '8', fruit: '葡萄' }, { id: '9', fruit: '柚子' }, { id: '10', fruit: '桃子' }, { id: '11', fruit: '西瓜' }, { id: '12', fruit: '哈密瓜' }, { id: '13', fruit: '榴莲' }, { id: '14', fruit: '芒果' }, { id: '15', fruit: '猕猴桃' }, { id: '16', fruit: '木瓜' }, { id: '17', fruit: '橙柚' }, { id: '18', fruit: '柠檬' }, { id: '19', fruit: '石榴' }, { id: '20', fruit: '橘子' }, { id: '21', fruit: '蜜柚' }, { id: '22', fruit: '杏子' }, { id: '23', fruit: '柿子' }, { id: '24', fruit: '青梅' }, { id: '25', fruit: '山竹' }, { id: '26', fruit: '百香果' }, { id: '27', fruit: '桑葚' }, { id: '28', fruit: '杨梅' }, { id: '29', fruit: '荔枝' }, { id: '30', fruit: '番茄' }]
-// const navList: string[] = ['全部', '香蕉', '苹果', '橙子', '草莓', '蓝莓', '菠萝', '樱桃', '葡萄', '柚子', '桃子', '西瓜', '哈密瓜', '榴莲', '芒果', '猕猴桃', '木瓜', '橙柚', '柠檬', '石榴', '橘子', '蜜柚', '杏子', '柿子', '青梅', '山竹', '百香果', '桑葚', '杨梅', '荔枝', '番茄', '橙白果', '火龙果', '车厘子', '枇杷', '莓果', '李子', '瓜子', '红毛丹', '龙眼', '橄榄', '柚子橘', '山楂', '梨子', '蜜瓜', '香瓜', '金橘', '杨桃', '西红柿', '牛油果', '水梨', '蒲桃', '甘蔗']
+const navList1: Array<NavItem> = [{ id: '-1', fruit: '全部' }, { id: '11', fruit: '西瓜' }, { id: '12', fruit: '哈密瓜' }, { id: '13', fruit: '榴莲' }, { id: '14', fruit: '芒果' }, { id: '15', fruit: '猕猴桃' }, { id: '16', fruit: '木瓜' }, { id: '17', fruit: '橙柚' }, { id: '18', fruit: '柠檬' }, { id: '19', fruit: '石榴' }, { id: '20', fruit: '橘子' }, { id: '21', fruit: '蜜柚' }, { id: '22', fruit: '杏子' }, { id: '23', fruit: '柿子' }, { id: '24', fruit: '青梅' }, { id: '25', fruit: '山竹' }, { id: '26', fruit: '百香果' }, { id: '27', fruit: '桑葚' }, { id: '28', fruit: '杨梅' }, { id: '29', fruit: '荔枝' }, { id: '30', fruit: '番茄' }]
+const navList2: Array<NavItem> = [{ id: '-1', name: '全部' }, { id: '1', name: '香蕉' }, { id: '2', name: '苹果' }, { id: '3', name: '橙子' }, { id: '4', name: '草莓' }, { id: '5', name: '蓝莓' }, { id: '6', name: '菠萝' }, { id: '7', name: '樱桃' }, { id: '8', name: '葡萄' }, { id: '9', name: '柚子' }, { id: '10', name: '桃子' }, { id: '11', name: '西瓜' }, { id: '12', name: '哈密瓜' }, { id: '13', name: '榴莲' }, { id: '14', name: '芒果' }, { id: '15', name: '猕猴桃' }, { id: '16', name: '木瓜' }, { id: '17', name: '橙柚' }, { id: '18', name: '柠檬' }, { id: '19', name: '石榴' }, { id: '20', name: '橘子' }]
 interface Props {
   title?: string
 }
 const Play1: React.FC<Props> = (props) => {
   if (props.title) document.title = props.title
-  const navIndexRef = useRef<number>(0)
-  const [navIndex, useNavIndex] = useState<number>(0)
-  const followItemRef = useRef<HTMLDivElement>(null)
-  const navItemRefs = navList.map(() => useRef<HTMLDivElement>(null))
-
-  useEffect(() => {
-    navAssignment()
-    window.addEventListener('resize', windowResize)
-    return () => {
-      window.removeEventListener('resize', windowResize)
-    }
-  }, [])
-
-  /**
-   * @description: Nav点击
-   * @return {type}
-   */
-  const onClickNav = (index: number) => {
-    navIndexRef.current = index
-    useNavIndex(index)
-    navAssignment()
-  }
-
-  /**
-   * @description: 窗口改变
-   * @return {type}
-   */
-  const windowResize = () => {
-    navAssignment()
-  }
-
-  /**
-   * @description: Nav赋值
-   * @param {number} index
-   * @return {type}
-   */
-  const navAssignment = () => {
-    const navItem = navItemRefs[navIndexRef.current].current
-    const width = navItem?.offsetWidth
-    const height = navItem?.offsetHeight
-    const left = navItem?.offsetLeft
-    const top = navItem?.offsetTop
-    const followItem = followItemRef.current
-    if (followItem) {
-      followItem.style.width = width + 'px'
-      followItem.style.height = height + 'px'
-      followItem.style.left = left + 'px'
-      followItem.style.top = top + 'px'
-    }
-  }
 
   /**
    * @description: Nav点击处理
@@ -76,8 +25,8 @@ const Play1: React.FC<Props> = (props) => {
 
   return (
     <div className='play1-container p-10 flex justify-center'>
-      <NavFollow list={navList} navItemName="fruit" followType="bg" onClickNavItemHandler={onClickNavItemHandler} />
-      <NavFollow list={navList} navItemName="fruit" followType="border" navTextColor="text-gray-600" navTextActiveColor="text-[var(--theme-primary)]" followColor="border-[var(--theme-primary)]" onClickNavItemHandler={onClickNavItemHandler} />
+      <NavFollow list={navList1} navItemName="fruit" followType="bg" size="large" onClickNavItemHandler={onClickNavItemHandler} />
+      <NavFollow list={navList2} navItemName="name" color="#eb2f96" onClickNavItemHandler={onClickNavItemHandler} />
     </div>
   )
 }
