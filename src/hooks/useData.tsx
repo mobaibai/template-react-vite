4c6905481d18d5b99890e3a50c8c81b988708874
mobaibai <puresilent.com@gmail.com>
2023-08-09 14:09:34 +0800

    ✨ feat: 新增swr默认参数

--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -1,19 +1,26 @@
+import type { SWRConfiguration } from 'swr'
 import useSWR from 'swr'
 import { useAjax } from '@/lib/ajax'
 
 interface Props {
   method: "get" | "post"
   path: string | undefined
-  params: JSONValue
+  params?: JSONValue
+  swrConf?: SWRConfiguration
 }
 /**
  * @description: 设置数据
  * @param {type} method 请求方式
  * @param {type} path 请求地址
  * @param {type} params 请求参数
+ * @param {type} swrConf SWR 设置
  * @return {type}
  */
-export const useData = ({ method = 'get', path, params = {} }: Props) => {
+export const useData = ({ method = 'get', path, params = {}, swrConf = {
+  revalidateIfStale: false,
+  revalidateOnFocus: false,
+  revalidateOnReconnect: false
+} }: Props) => {
   const { get, post } = useAjax({ showLoading: true, handleError: true })
   const { data, mutate, isLoading, isValidating, error } = useSWR<DataType<ResponseDataListType | ItemType>>(
     path,
@@ -25,7 +32,8 @@ export const useData = ({ method = 'get', path, params = {} }: Props) => {
         await post<DataType<ResponseDataListType | ItemType>>(path, params)
 
       return res.data
-    }
+    },
+    swrConf
   )
 
   return {
