ae99d9c09fe4150dc717ffc2bf83832d5989b18f
mobaibai <puresilent.com@gmail.com>
2024-04-29 14:36:32 +0800

    🦄 refactor: 补全404页面插图

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,11 +1,28 @@
+import { NavLink } from 'react-router-dom'
+import { Button } from 'antd'
 import { useTitle } from '@/hooks/useTitle'
+import page404 from '@/assets/bgs/page404.svg'
 
-interface Props {
+type Props = {
   title?: string
 }
 const EmptyPage: React.FC<Props> = props => {
   if (props.title) useTitle(props.title)
 
-  return <div className='empty-page-container'>404：当前页面不存在!</div>
+  return (
+    <div className='empty-page-container h-screen flex flex-col center space-y-50px'>
+      <div className='empty flex flex-col center space-y-20px'>
+        <div className='img'>
+          <img className='h-50' src={page404} alt='' />
+        </div>
+        <div className='text text-14px c-#ccc'>页面迷路了</div>
+      </div>
+      <div className='nav'>
+        <NavLink className='c-[var(--theme-primary)]' to={'/'}>
+          <Button type='primary'>返回首页</Button>
+        </NavLink>
+      </div>
+    </div>
+  )
 }
 export default EmptyPage
